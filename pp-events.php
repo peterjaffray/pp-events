be5f292f68d6401e27cd0d11ebec1afc2d74f2b8
peterjaffray <peter@choice.marketing>
2020-11-13 12:46:07 -0600

    Moved the tracker and established core componenets for WordPress plugin functionality

--- pp-events.php
+++ pp-events.php
@@ -5,13 +5,30 @@
  * Description: The very first plugin that I have ever created.
  * Version: 1.0
  * Author: Peter Jaffray and Pablo Reategui
-<<<<<<< HEAD
  * Company: Gustin Quon
-=======
  * 
  *
  *
->>>>>>> 4b2e6c336e912550f00c56c4ef05f623a0a897b2
  */
+
+function pp_enqueue($hook) {
+	
+	// wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+	//
+	// create version based on file modification time
+	//
+
+	$my_js_ver  = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'js/tracker.js' ));
+	$my_css_ver = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'style.css' ));
+
+	wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+
+	wp_enqueue_script( 'pp_tracker_js', plugins_url( 'js/tracker.js', __FILE__ ), array(), $my_js_ver );
+        wp_register_style( 'pp_css',    plugins_url( 'style.css',    __FILE__ ), false,   $my_css_ver );
+	wp_enqueue_style ( 'pp_css' );
+
+}
+
+add_action('wp_enqueue_scripts', 'pp_enqueue');
 
 
