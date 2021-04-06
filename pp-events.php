8e32c90ba404b08e3461a4120d5119fa9ff117f9
Peter Jaffray <peter@choiceomg.com>
2021-04-06 15:08:51 -0500

    Update pp-events.php

--- pp-events.php
+++ pp-events.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: My First Plugin
- * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Plugin Name: Powerful Event Tracking
+ * Plugin URI: 
  * Description: The very first plugin that I have ever created.
  * Version: 1.0
  * Author: Peter Jaffray and Pablo Reategui
